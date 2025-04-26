f853c89b6ac7565627ab3ee3b4cadc105e8c556d
jpig <isdolinskayaym@gmail.com>
2025-04-26 21:01:18 +0200

    moved words generation to server

--- src/engine/game.ts
+++ src/engine/game.ts
@@ -123,6 +123,179 @@ export class GameEngine implements IGame {
 			this.state.players[this.state.activePlayer].secret!,
 		);
 	}
+	
+	private generateWords = () => {
+		const data: IWords = this.state.turn!.droppedLetters.reduce((acc, droppedLetterId) => {
+		
+		let position = {y: 0, x: 0};
+		for (let i = 0; i < this.state.field.length; i++) {
+			for (let j = 0; j < this.state.field[i].length; j++) {
+				if (this.state.field[i][j] === droppedLetterId) {
+					position.y = i;
+					position.x = j;
+					break;
+				}
+			}
+		}
+
+		if (Object.keys(acc).length) {
+			const { changedWords } = this.updateCurrentWords(acc, droppedLetterId, position);
+			const newWords = this.makeNewWords(droppedLetterId, position);
+			acc = { ...changedWords, ...newWords };
+		} else {
+			const newWords = this.makeNewWords(droppedLetterId, position);
+			acc = { ...newWords };
+		}
+			
+		return acc;
+		}, {});
+
+		const res = Object.values(data).map(item => {
+			const {letterIds} = item;
+
+			const word = letterIds.map(letterId => this.state!.letters![letterId].value);
+
+			return {...item, word };
+		});
+
+		console.log('----WORDS: ----', res);
+	}
+
+	private makeNewWords = (letterId: string, position: { x: number; y: number }) => {
+		const verticalWord = this.makeWord(letterId, position, 'y');
+		const horizontalWord =this.makeWord(letterId, position, 'x');
+		let response: IWords = {};
+
+		if (!Object.keys(verticalWord).length && !Object.keys(horizontalWord).length) {
+			// слово из одной буквы
+			const wordId = [letterId].join(';');
+
+			response[wordId] = {
+				start: `${position.x};${position.y}`,
+				letterIds: [letterId],
+				kind: 'vertical'
+			}
+		}
+
+		if (Object.keys(verticalWord).length) {
+			response = { ...response, ...verticalWord };
+		}
+
+		if (Object.keys(horizontalWord).length) {
+			response = { ...response, ...horizontalWord };
+		}
+
+		return response;
+	}
+
+	private makeWord = (letterId: string, startPosition: { x: number; y: number }, axis: 'y' | 'x') => {
+		const letters = [letterId];
+		const topLimit = 0;
+		const bottomLimit = this.state.field.length - 1;
+		let wordStartPosition = `${startPosition.x};${startPosition.y}`;
+		let up = startPosition[axis] > topLimit;
+
+		const result: IWords = {};
+
+		const walk = (pos: { x: number; y: number }): any => {
+			const position = { ...pos };
+			if (up) {
+				--position[axis];
+
+				const data = this.state.field[position.y][position.x];
+				const isLetter = data && !isNaN(Number(data));
+
+				if (!isLetter) {
+					up = false;
+					return walk(startPosition);
+				}
+
+				wordStartPosition = `${position.x};${position.y}`;
+				letters.unshift(data);
+
+				return walk(position);
+			}
+			if (position[axis] === bottomLimit) {
+				return;
+			}
+
+			++position[axis];
+
+			const data = this.state.field[position.y][position.x];
+			const isLetter = data && !isNaN(Number(data));
+
+			if (!isLetter) {
+				return;
+			}
+
+			letters.push(data);
+			return walk(position);
+
+		}
+
+		walk(startPosition);
+
+		if (letters.length > 1) {
+			const wordId = letters.join(';');
+
+			result[wordId] = {
+				start: wordStartPosition,
+				letterIds: letters,
+				kind: axis === 'y' ? 'vertical' : 'horizontal'
+			}
+		}
+
+		return result;
+	}
+
+	private updateCurrentWords = (data: IWords, letterId: string, position: { x: number; y: number }) => {
+		const newLetterPosition = `${position.x};${position.y}`;
+
+		const newWords = Object.keys(data).reduce<IWords>((acc, wordId) => {
+			const word = data[wordId];
+			const { start, letterIds, kind } = word;
+
+			const newWord = { ...word };
+
+			const [x, y] = start.split(';');
+
+			let upperLetterPosition;
+			let bottomLetterPosition;
+
+			if (kind === 'vertical') {
+				const verticalLimit = Number(y) + (letterIds.length - 1);
+
+				upperLetterPosition = `${x};${Number(y) - 1}`;
+				bottomLetterPosition = `${x};${Number(verticalLimit) + 1}`;
+
+			} else {
+				const horizontalLimit = Number(x) + (letterIds.length - 1);
+				upperLetterPosition = `${Number(x) - 1};${y}`;
+				bottomLetterPosition = `${Number(horizontalLimit) + 1};${y}`;
+			}
+
+			if (newLetterPosition === upperLetterPosition || newLetterPosition === bottomLetterPosition) {
+				if (newLetterPosition === upperLetterPosition) {
+					newWord.start = newLetterPosition;
+					newWord.letterIds.unshift(letterId);
+				} else if (newLetterPosition === bottomLetterPosition) {
+					newWord.letterIds.push(letterId);
+				}
+
+				delete acc[wordId];
+				const newId = newWord.letterIds.join(';');
+
+				acc[newId] = newWord;
+			} else {
+
+				acc[wordId] = newWord;
+			}
+
+			return acc;
+		}, {});
+
+		return { changedWords: newWords };
+	}
 
 	public addLetter(payload: IAddLetter) {
 		const { position, letterId } = payload;
@@ -130,6 +303,8 @@ export class GameEngine implements IGame {
 		this.state.field[position.y][position.x] = letterId;
 
 		this.state.turn?.droppedLetters.push(letterId);
+
+		this.generateWords();
 		
 		this.eventBus.emit(EVENTS.UPDATE_TURN_LETTERS, {dropppedLetters: this.state.turn?.droppedLetters, sessions: this.sessions});
 		this.eventBus.emit(EVENTS.UPDATE_TURN_FIELD, { field: this.state.field, sessions: this.sessions });
